c931230bd5f58a4e83e7900017ac3d6228685147
Meldir <134066210+portfolio-dev@users.noreply.github.com>
2024-10-17 20:58:11 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -43,10 +43,3 @@
 //   }
 // });
 
-document.addEventListener("DOMContentLoaded", function() {
-            var link = document.createElement("link");
-            link.rel = "icon";
-            link.type = "image/png"; // Ganti sesuai format favicon
-            link.href = "/images/favicon.png"; // Ganti dengan path yang benar
-            document.head.appendChild(link);
-        });
